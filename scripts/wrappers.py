5044f95d16182256475feba38cad435bd253e1aa
Pablo Alonso Alguacil <alonsoap@unican.es>
2025-10-24 18:02:46 +0000

    Fix post-process plot

--- scripts/wrappers.py
+++ scripts/wrappers.py
@@ -13,3 +13,13 @@ class SwashModelWrapper_ondas(SwashModelWrapper):
         # Save depth.bot
         np.savetxt(op.join(case_dir, "depth.bot"), self.depth_array)
  
+class SwashModelWrapper_shoaling(SwashModelWrapper):
+    """
+    Wrapper for the SWASH model with vegetation.
+    """
+
+    def build_case(self, case_context: dict, case_dir: str) -> None:
+        super().build_case(case_context=case_context, case_dir=case_dir)
+
+        # Save depth.bot
+        np.savetxt(op.join(case_dir, "depth.bot"), self.depth_array)
